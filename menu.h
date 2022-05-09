07948a08979df0837ded41c85c77e1dc26e647f6
MinJe21 <103713308+MinJe21@users.noreply.github.com>
2022-05-09 19:51:24 +0900

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,16 +1,19 @@
-typedef struct{
-    char name[10]; //예약자 이름
-    int time; //사용할 시간
-    int type; // 좌석종류(1. 오픈형 2. 1인실)
-    char num[10]; //좌석 번호
-}Menu;
+    #include <stdio.h>
+    #include <string.h>
+    typedef struct{
+        char name[10]; //예약자 이름
+        int time; //사용할 시간
+        int type; // 좌석종류(1. 오픈형 2. 1인실)
+        char num[10]; //좌석 번호
+    }Menu;
 
-int addMenu(Menu *s);
-void readMenu(Menu s);
-void listMenu(Menu *s, int count);
-int selectDataNo(Menu *s, int count);
-int updateMenu(Menu *s);
-int deleteMenu(Menu *s);
-void saveData(Menu *s, int count);
-int loadData(Menu *s);
-int selectMenu();
+    int addMenu(Menu *s);
+    void readMenu(Menu s);
+    void listMenu(Menu *s, int count);
+    int selectDataNo(Menu *s, int count);
+    int updateMenu(Menu *s);
+    int deleteMenu(Menu *s);
+    void saveData(Menu *s, int count);
+    int loadData(Menu *s);
+    void searchName(Menu *s, int count);
+    int selectMenu();
